c7100505234c1a96576f92a6a957d4f999192826
S Mautner <mautner@cs.uni-freiburg.de>
2023-02-27 22:41:08 +0000

    lucy geneselect

--- lucy/adatas.py
+++ lucy/adatas.py
@@ -6,22 +6,17 @@ import logging
 import ubergauss.tools as ut
 
 class mergeutils:
-
-
     def confuse2(self, labels):
         assert self.sorted
         assert len(self.data) == 2, 'i could just assume that you mean the first 2 datasets, but i refuse :D'
         assert len(labels[0])== self.data[0].shape[0]
         assert len(labels[1])== self.data[1].shape[0]
         confuse2(labels)
-
-
     def plot(self, labels, **kwargs):
         plot_merge(self, labels,**kwargs)
 
 
 default_hvg_name = 'cell_ranger'
-
 
 def jaccard_distance(a,b, num_genes):
     binarized_hvg = np.array([ ut.binarize(d,num_genes) for d in [a,b] ])
@@ -77,9 +72,7 @@ class Merge(mergeutils):
         if make_even:
             self.data = subsample_to_min_cellcount(self.data)
 
-
         ######################
-
 
         if pca:
             self.PCA = self.projections[1]
@@ -140,21 +133,30 @@ def subsample_to_min_cellcount(adatas):
         return adatas
 
 
-def add_pca(adatas, dim):
-    # maybe we need to check if already done?
+def add_pca(adatas, dim, label = 'pca'):
+
+    if not label:
+        label = 'pca'+str(dim)
+    if label in adatas[0].obsm:
+        return adatas
+
     data = stack(adatas)
     scaled = sc.pp.scale(data, zero_center=False, copy=True,max_value=10).X
     res = decomposition.PCA(n_components  = dim).fit_transform(scaled)
-    data.obsm[] = res # TODO needs name
+    data.obsm[label] = res
     return unstack(data)
 
 
-def add_umap(adatas, dim):
-    # maybe we need to check if already done?
-    data = stack(adatas)
-    # TODO we might start from pca..
-    res = umap.UMAP(n_components = dim).fit_transform()
-    data.obsm[] = res # TODO needs name
+def add_umap(adatas, dim, label = '', start = 'pca40'):
+
+    if not label:
+        label = 'umap'+str(dim)
+    if label in adatas[0].obsm:
+        return adatas
+
+    X = data.obsm.get(start,data.X)
+    res = umap.UMAP(n_components = dim).fit_transform(X)
+    data.obsm[label] = res
     return unstack(data)
 
 
@@ -176,3 +178,32 @@ def sort_cells(adatas, ):
 def hungarian(self,data_fld,data_id, data_id2):
         hung, dist = mergehelpers.hungarian(self.projections[data_fld][data_id],self.projections[data_fld][data_id2])
         return hung, dist[hung]
+
+
+
+
+def cell_ranger(adatas, mingenes = 200,
+                        normrow= True,
+                        log = True):
+    if 'cell_ranger' in adatas[0].varm:
+        return adatas
+
+    return Map( lambda x:cell_ranger_single(x, mingenes=mingenes, normrow= normrow,  log= log), adatas)
+
+def cell_ranger_single(adata,
+                        mingenes = 200,
+                        normrow= True,
+                        log = True):
+
+    sc.pp.normalize_total(adata, 1e4)
+    sc.pp.log1p(adata)
+    sc.pp.highly_variable_genes(adata, n_top_genes=5000,
+                                         flavor='cell_ranger',
+                                        inplace=True)
+    adata.varm['cell_ranger']=  data.var['dispersions_norm']
+    return adata
+
+
+
+
+
