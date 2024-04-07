7711676f683f49fcc866ad0c7dcc6fb74be4e22a
S Mautner <mautner@cs.uni-freiburg.de>
2024-04-07 21:19:35 +0000

    demo to show how we react to shifting and added rotation to the scoring

--- application/artificial_example.py
+++ application/artificial_example.py
@@ -24,8 +24,7 @@ def scplot(dataset):
 
 def makedata():
     # dataset = scalp.data.mkblobs(sizes = [[24,21],[25,20],[23,22]], clusterspread = .3, batchspread = 4)
-    dataset = scalp.data.mkblobs(sizes = [[40,24,21],[40,25,20],[40,23,22]],
-                                 clusterspread = .5, batchspread = 1.5)
+    dataset = scalp.data.mkblobs(sizes = [[40,24,21],[40,25,20],[40,23,22]], clusterspread = .5, batchspread = 1.5)
     #dataset = scalp.data.mkblobs(sizes = [[3,5],[5,3]], clusterspread = .1, batchspread = 4)
     # for d in dataset: d.obsm['umap'] = d.X
     scplot(dataset)
@@ -37,47 +36,86 @@ def runscalp(dataset):
              'add_tree': False,
              'epsilon'  : 1e-4,
               'copy_lsa_neighbors': False,
-             'inter_outlier_threshold': .9,
+             'inter_outlier_threshold': .5,
              'inter_outlier_probabilistic_removal': False}
     dataset, graph = scalp.mkgraph(dataset,**parm)
     scalp.umapwrap.graph_umap(dataset,graph,label = 'umap', n_components = 2)
     return dataset
 
 def heatmap(df, val = 'lin'):
-    '''
-    - pivot targets
-    - plot
-    '''
-    sns.heatmap(df.pivot(index = 'cspread', columns = 'bspread', values= val))
+    sns.heatmap(df.pivot(index = 'cspread', columns = 'degree', values= val))
     plt.show()
 
 
 from ubergauss import optimization as uo
 def run_scores():
-    def f(_,cspread=0,bspread=0):
-        dataset = scalp.data.mkblobs(sizes = [[40,24,21],[40,25,20],[40,23,22]],
+    def f(_,cspread=0,bspread=6,degree = 0):
+
+        dataset = scalp.data.mkblobs(sizes =
+                                     [[40,24,21],[20,45,20],[20,23,42]],
                                      clusterspread = cspread,
                                      batchspread = bspread)
+        dataset[1]=rotate(dataset[1],degree=degree)
         dataset = runscalp(dataset)
         dataset = scalp.transform.stack(dataset)
         return {'batch' : score.score_lin_batch(dataset),
                 'lin': score.score_lin(dataset)}
 
-    pdict = {'cspread' :[.5,.75], 'bspread' :[.5,2,8]}
+    pdict = {'cspread' :np.arange(.25,4,.25), 'degree' :np.arange(0, 200, 20 )}
 
     df = uo.gridsearch(f,param_dict = pdict, data = [1])
     heatmap(df, 'lin')
     heatmap(df,'batch')
+    return df
 
 import pandas as pd
 def pareto(df):
-    df = df.melt(id_vars = ['cspread','bspread'],
+    df = df.melt(id_vars = ['cspread','degree'],
                  value_vars=['batch','lin'],
                  var_name='target', value_name='score')
-    nus = uo.pareto_scores(df,method = 'cspread bspread'.split(' '), data = None)
-    stuff  = [dict(zip('cspread bspread score'.split(' '),[c,b,score])) for (c,b),score in nus]
+    nus = uo.pareto_scores(df,method = 'cspread degree'.split(' '), data = None)
+    stuff  = [dict(zip('cspread degree score'.split(' '),[c,b,score])) for (c,b),score in nus]
     stuff = pd.DataFrame(stuff)
     heatmap(stuff,'score')
     return stuff
+
+'''
+we shift one batch to the right and see how the score is affected
+-> just shifting a batch sideways doesnt break the hungarian
+'''
+def shift_test():
+    dataset = scalp.data.mkblobs(sizes = [[40,24,21],[40,25,20],[40,23,22]],
+                                 clusterspread = .5, batchspread = 6)
+
+    runscalp(dataset)
+    scplot(dataset)
+
+    dataset[1].X[:,0] += 40
+    scplot(dataset)
+    runscalp(dataset)
+    scplot(dataset)
+
+'''
+test tehe rotation
+'''
+def rotate(anndata, degree=90):
+    center = anndata.X.mean(axis=0)
+    anndata.X -= center
+    angle = degree/180*np.pi
+    rot = np.array([[np.cos(angle),-np.sin(angle)],
+                    [np.sin(angle),np.cos(angle)]])
+    anndata.X = anndata.X @ rot
+    anndata.X+= center
+    anndata.obsm['umap']= anndata.X
+    return anndata
+
+
+def rotation_test():
+    dataset = scalp.data.mkblobs(sizes = [[40,24,21],[40,25,20],[40,23,22]],
+                                 clusterspread = .5, batchspread = 6)
+    dataset[1] = rotate(dataset[1])
+    scplot(dataset)
+
+
 
 
